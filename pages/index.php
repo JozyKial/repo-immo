f6ac79cb9682b67e3f853bfea7e3f266ea02a785
vwez <desirv63@gmail.com>
2020-12-11 02:55:36 +0100

    commit de la prmière mouture patie siteweb pas vwez

---
+++ pages/index.php
@@ -0,0 +1,92 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="Dashboard">
+  <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+  <title>Titre du projet</title>
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  
+
+  
+
+  <link href="../css/frontStyle.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Template Name: Dashio
+    Template URL: https://templatemag.com/dashio-bootstrap-admin-template/
+    Author: TemplateMag.com
+    License: https://templatemag.com/license/
+  ======================================================= -->
+ </head>
+
+<body>
+
+<nav class="navbar sticky-top navbar-expand-sm navbar-light" id="ombre" >
+  
+    <a class="navbar-brand" href="#">Trouvez votre bonheur</a>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item ">
+          <button class="btn btn-outline-secondary btn-sm" id="deposer"> Déposer une annonce</button> 
+        </li>
+      </ul>
+    </div>
+
+      
+      <ul class="navbar-nav ml-md-auto">
+        
+        <li class="nav-item">
+          <a class="nav-item nav-link mr-md-2" href="#" > Recommendation  </a> 
+        </li>
+        <li class="nav-item">
+          <a class="nav-link "  target="_blank" rel="noopener" >Mes favoris</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link "  target="_blank" rel="noopener" >Créer un compte</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link "  target="_blank" rel="noopener" >Se conecter</a>
+        </li>
+        <li class="nav-item dropleft">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Menu
+            </a>
+
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Acheter</a>
+              <a class="dropdown-item" href="#">Louer</a>
+              <a class="dropdown-item" href="#">Construire</a>
+              <a class="dropdown-item" href="#">Bureaux & Commerces</a>
+              <a class="dropdown-item" href="#">Emprunter</a>
+              <a class="dropdown-item" href="#">Emmenager</a>
+              <a class="dropdown-item" href="#">Estimer</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Poser ma question</a>
+            </div>
+          </li>
+        
+        
+      </ul>
+  
+     
+        
+      
+    </nav>
+    
+    <div >
+      <img src="../img/baner2.jpg" alt="" srcset="" class="baner">
+    </div>
+  
+</body>
+ 
+
+</html>
